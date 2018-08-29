418660882f1afd6a1130e30f5d00152b2ecc9055
tanirocr@gmail.com <alakazamxdgithub12>
2018-08-29 08:53:05 -0300

    update2018

--- app/src/main/java/br/ufrn/eaj/tads/webviewexample/MainActivity.java
+++ app/src/main/java/br/ufrn/eaj/tads/webviewexample/MainActivity.java
@@ -8,23 +8,18 @@ import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.CheckBox;
 import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
     WebView webview;
     ProgressBar progressBar;
-    SeekBar sbDiscrete;
-    SeekBar sbNormal;
-    RatingBar ratingBar;
-
-    TextView tvDiscrete;
-    TextView tvNormal;
-    TextView tvRating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +29,6 @@ public class MainActivity extends AppCompatActivity {
 
         webview = (WebView) findViewById(R.id.webView);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        sbDiscrete = (SeekBar) findViewById(R.id.seekBarDiscrete);
-        sbNormal = (SeekBar) findViewById(R.id.seekBarNormal);
-        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
-
-        tvDiscrete = (TextView) findViewById(R.id.textViewDiscrete);
-        tvNormal = (TextView) findViewById(R.id.textViewNormal);
-        tvRating = (TextView) findViewById(R.id.textViewRating);
 
         webview.loadUrl("http://tads.eaj.ufrn.br");
         webview.setWebViewClient(new WebViewClient() {
@@ -69,49 +57,5 @@ public class MainActivity extends AppCompatActivity {
         webview.setWebChromeClient(new WebChromeClient());
         webview.getSettings().setJavaScriptEnabled(true);
         webview.loadUrl("javascript:alert('Olá mundo');");
-
-        //***********************************************************************
-
-        sbDiscrete.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                tvDiscrete.setText(""+i);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        sbNormal.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                tvNormal.setText(""+i);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
-                tvRating.setText(""+v);
-            }
-        });
-
     }
 }
